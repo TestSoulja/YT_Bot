9f6ea3ef2b20fac18b72bb5414a51a1c7af140f1
TestSoul <s.ekker@zmeke.com>
2022-08-20 21:52:27 +0300

    ///

--- main_stage.py
+++ main_stage.py
@@ -135,6 +135,22 @@ def func(message):
 		z = message.text
 
 
-# @app.route("/", methods=["POST"])port=APP_PORT, debug=True)
+# @app.route("/", methods=["POST"])
+# def webhook():
+# 	if flask.request.headers.get("content-type") == "application/json":
+# 		json_string = flask.request.get_data().decode("utf-8")
+# 		update = telebot.types.Update.de_json(json_string)
+# 		bot.process_new_updates([update])
+# 		return ""
+# 	else:
+# 		flask.abort(403)
+#
+#
+# if __name__ == "__main__":
+# 	bot.remove_webhook()
+# 	time.sleep(1)
+# 	bot.set_webhook(url=WEB_HOOK_URL)
+# 	APP_PORT = int(APP_PORT)
+# 	app.run(host=APP_HOST, port=APP_PORT, debug=True)
 
 bot.polling(none_stop=True, interval=0)
